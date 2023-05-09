49cd4a206e01a36566727495e60ae8bc6d70a060
ildaneta <iildaemanoely@gmail.com>
2023-05-09 21:05:31 +0200

    feat: add storage into auth configs in supabase

--- src/lib/initSupabase.ts
+++ src/lib/initSupabase.ts
@@ -2,5 +2,13 @@ import { createClient } from '@supabase/supabase-js';
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from './contants';
 import 'react-native-url-polyfill/auto'
 import { Database } from '../types/supabase';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false
+  },
+});
